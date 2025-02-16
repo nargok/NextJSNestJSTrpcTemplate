7e01e8e9f96b9799d79b52742459bba29e0aee6b
nargok <nargok@gmail.com>
2025-02-16 23:41:15 +0900

    delete trpc module

--- apps/backend/src/trpc/trpc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrpcRouter } from './trpc.router';
-import { TrpcService } from './trpc.service';
-
-@Module({
-  providers: [TrpcService, TrpcRouter],
-  exports: [TrpcService, TrpcRouter],
-})
-export class TrpcModule {}
