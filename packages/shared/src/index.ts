cdd594990ae0281076c329894a2a65375c56ae61
nargok <nargok@gmail.com>
2025-02-17 22:17:52 +0900

    use trpc in web

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,9 +1,12 @@
+// tRPC will look for this file to find shared types
+// @ts-ignore
+export type { AppRouter } from '../../apps/backend/src/@generated/server';
+
+// Import types from other files
+export * from './types/user';
+
 // Shared types will be defined here
 export type ExampleType = {
   id: string;
   name: string;
 };
-
-// export type { TrpcRouter } from '../../apps/backend/src/trpc/trpc.router';
-
-export * from './types/user';
