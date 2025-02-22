8e00a4ef8e031399991e3b3acf05703db978bbdd
nargok <nargok@gmail.com>
2025-02-22 15:54:55 +0900

    delete src from root

--- src/domain/repositories/user.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../entities/user.entity';
-
-export const USER_REPOSITORY = 'USER_REPOSITORY';
-
-export interface IUserRepository {
-  findAll(): Promise<User[]>;
-} 
--- src/presentation/controllers/users.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from '../../application/services/users.service';
-
-@Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-} 
